b266ac7b53dc4fb83cf0918064f2ff7a0547e2f9
Daviddevlinux <daviddevmaia@gmail.com>
2024-11-26 10:58:28 -0300

    LAB3 ok

--- ADS-LAB3/script_q2a.sh
+++ ADS-LAB3/script_q2a.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
+
 BASE_OUTPUT_DIR="results_q2a"
 mkdir -p $BASE_OUTPUT_DIR
 
 
-VALOR_MAXIMO=930000
-TAMANHOS_ENTRADA=(9300000 93000000) 
-N=50  # Substitua pelo valor calculado na 1a
-ALGORITMOS=("QUICK" "MERGE" "COUNTING")
-
+VALOR_MAXIMO=930000  
+TAMANHOS_ENTRADA=(9300000 93000000)  
+N=50  # Número de medições
+ALGORITMOS=("QUICK" "MERGE" "COUNTING")  
 
 for TAMANHO_ENTRADA in "${TAMANHOS_ENTRADA[@]}"; do
     for ALGORITMO in "${ALGORITMOS[@]}"; do
-       
+        
         ALGORITMO_DIR="$BASE_OUTPUT_DIR/${ALGORITMO}_size_${TAMANHO_ENTRADA}"
         mkdir -p $ALGORITMO_DIR
 
-        echo "Executando $N medições para $ALGORITMO com tamanho de entrada $TAMANHO_ENTRADA"
+        echo "Executando $N medições para $ALGORITMO com tamanho de entrada $TAMANHO_ENTRADA e valor máximo $VALOR_MAXIMO"
         for i in $(seq 1 $N); do
-            echo "Medição $i para $ALGORITMO com tamanho $TAMANHO_ENTRADA..."
-            java -cp bin MedidorDeOrdenacao $ALGORITMO $TAMANHO_ENTRADA $VALOR_MAXIMO >> "$ALGORITMO_DIR/medicao_$i.txt"
+            echo "Medição $i para $ALGORITMO com tamanho de entrada $TAMANHO_ENTRADA..."
+           
+            java -cp "bin:." MedidorDeOrdenacao $ALGORITMO $TAMANHO_ENTRADA $VALOR_MAXIMO >> "$ALGORITMO_DIR/medicao_$i.txt"
+
         done
-        echo "Resultados para $ALGORITMO com tamanho $TAMANHO_ENTRADA salvos no diretório $ALGORITMO_DIR"
+        echo "Resultados para $ALGORITMO com tamanho de entrada $TAMANHO_ENTRADA salvos no diretório $ALGORITMO_DIR"
     done
 done
 
